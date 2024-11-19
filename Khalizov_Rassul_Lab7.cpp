06209a726135498809502879d99756ef7a36fd73
Rassul-creator-commits <rassulk@nycstudents.net>
2024-11-18 20:21:49 -0500

    Update Khalizov_Rassul_Lab7.cpp

--- Khalizov_Rassul_Lab7.cpp
+++ Khalizov_Rassul_Lab7.cpp
@@ -1,108 +1,48 @@
-#include <fstream>
-#include <iostream>
-#include <iomanip>
-#include <string>
+#include <iostream>   // enables inputs and outputs operations
+#include <fstream> 
+#include <string> 
 
-using namespace std;
+using namespace std; 
 
-int main()
-{
-    // Defines variables
-    ifstream game_scores; 
-    ofstream outgame_scores;
-    string playername1;
-    double score1_1;
-    double score1_2;
-    double score1_3;
+int main () {
+    ifstream game_scores; // make game_scores the variable that allows you to input stuff into the txt file
+    ofstream outgame_scores; // outputs the results into this txt file
 
-    double total_1;
+    game_scores. open("game_scores.txt"); 
+    outgame_scores. open("outgame_score.txt"); // opens the txt file
 
-    string playername2;
-    double score2_1;
-    double score2_2;
-    double score2_3;
+    double score1, score2, score3;
+    string name; 
+    while(game_scores >> name >> score1 >> score2 >> score3){
+        // allows for the input of score1, score2, score3 from the game_scores.txt
 
-    double total_2;
+        // calculates the average by multiplying the numbers by the amount of percent that they are worth and then adding them together
+        double average = score1 * 0.2 + score2 * 0.3 + score3 * 0.5;
 
-    string playername3;
-    double score3_1;
-    double score3_2;
-    double score3_3;
-
-    double total_3;
-
-
-
-    game_scores.open("games.txt"); //opens the file named inData.txt
-    outgame_scores.open("outgame_scores.txt");  // opens the file named outData.txt
-
-    game_scores >> playername1 >> score1_1 >> score1_2 >> score1_3; 
-    total_1 = (score1_1 * 0.2) + (score1_2 * 0.3) + (score1_3 * 0.5);
-
-    if(total_1 < 1000){
-        outgame_scores << playername1 << " Beginner - Keep Practicing!" << endl; 
-
-    } else if(total_1 >= 1000 && total_1 < 1500){
-        outgame_scores << playername1 << " Intermediate" << endl; 
-    
-    } else if (total_1 >= 1500 && total_1 < 1800){
-        outgame_scores << playername1 << "Advanced - Good Job!" << endl;
-
-    } else if (total_1 >= 1800 && total_1 < 2000){
-        outgame_scores << playername1 << "Master - Good Job!" << endl; 
-
-    } else if(total_1 > 2000){
-        outgame_scores << playername1 << "Congrats! You are an Expert!" << endl; 
-
-    } else{
-        outgame_scores << "input the player name, first score, second score, third score" << endl; 
+        // if the average score is greather than 2000, it prints out that message in the txt file 
+        if(average > 2000){
+            outgame_scores << name << " Congrats! You are an Expert!" << endl; 
+            }
+            // if the average score is equal to and less than and also greather than 1800, the following message is printed
+            else if(average <= 2000 && average > 1800){
+            outgame_scores << name << " Master - Good Job!" << endl; 
+            }
+            // if the average score is euqal to and less than but also greater than 1500, the following message is printed 
+            else if(average <= 1800 && average > 1500){
+            outgame_scores << name << " Advanced - Good Job!" << endl;
+            }
+            // if the average score is equal to and less than 1600 but also greater than 1000, the following message is printed
+            else if(average <= 1500 && average > 1000){
+            outgame_scores << name << " Intermediate" << endl;
+            }
+            //  if the average is less than 1000, the following message is printed
+            else if(average < 100){
+            outgame_scores << name << " Beginner - Keep Practicing!" << endl;
+            }     
     }
-
-
-    game_scores >> playername2 >> score2_1 >> score2_2 >> score2_3; 
-    total_2 = (score2_1 * 0.2) + (score2_2 * 0.3) + (score2_3 * 0.5);
-
-    if(total_2 < 1000){
-        outgame_scores << playername2 << " Beginner - Keep Practicing!" << endl; 
-
-    } else if(total_2 >= 1000 && total_2 < 1500){
-        outgame_scores << playername2 << " Intermediate" << endl; 
-
-    } else if (total_2 >= 1500 && total_2 < 1800){
-        outgame_scores << playername2 << "Advanced - Good Job!" << endl;
-
-    } else if (total_2 >= 1800 && total_2 < 2000){
-        outgame_scores << playername2 << "Master - Good Job!" << endl; 
-
-    } else if(total_2 > 2000){
-        outgame_scores << playername2<< "Congrats! You are an Expert!" << endl; 
-
-    } else{
-        outgame_scores << "input the player name, first score, second score, third score" << endl; 
-    }
-
-    game_scores >> playername3 >> score3_1 >> score3_2 >> score3_3; 
-    total_3 = (score3_1 * 0.2) + (score3_2 * 0.3) + (score3_3 * 0.5);
-
-    if(total_3 < 1000){
-        outgame_scores << playername3 << " Beginner - Keep Practicing!" << endl; 
-
-    } else if(total_3 >= 1000 && total_3 < 1500){
-        outgame_scores << playername3 << " Intermediate" << endl; 
-
-    } else if (total_3 >= 1500 && total_3 < 1800){
-        outgame_scores << playername3 << "Advanced - Good Job!" << endl;
-
-    } else if (total_3 >= 1800 && total_3 < 2000){
-        outgame_scores << playername3 << "Master - Good Job!" << endl; 
-
-    } else if(total_3 > 2000){
-        outgame_scores << playername3 << "Congrats! You are an Expert!" << endl; 
-
-    } else{
-        outgame_scores << "input the player name, first score, second score, third score" << endl; 
-    }
-
-    
-    return 0; 
+    /* John Congrats! You are an Expert!
+    Jack Beginner - Keep Practicing!
+    Jacob Congrats! You are an Expert!
+    */
+    return 0;
 }
